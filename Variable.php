2cceaa0b93fcbe7d02e38df26244df6795cea855
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-13 09:29:45 +0100

    First_Variables_Upload

---
+++ Variable.php
@@ -0,0 +1,24 @@
+
+<HTML>
+
+<Body>
+
+<?php
+
+$myText1="Good Morning Campers!";
+$myText2="Suits you sir!";
+
+
+
+echo " The first string is:  '";
+echo $myText1;
+echo "'<br/><br/>";
+echo "The second string is:  '";
+echo $myText2;
+echo "'<br/><br/>";
+
+?>
+
+</body
+
+</HTML>
