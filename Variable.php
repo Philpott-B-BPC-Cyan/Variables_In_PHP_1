1aa5e9a92a44859210876c85a14e2c195c6bcc54
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-13 10:12:39 +0100

    Now

--- Variable.php
+++ Variable.php
@@ -9,10 +9,10 @@ $myText2="Suits you sir";
 
 
 
-echo "The first string is:   '";
+echo "The first string is:   ";
 echo $myText1;
 echo "'<br/><br/>'";
-echo "The second string is:  '";
+echo "The second string is:  ";
 echo $myText2;
 echo "'<br/><br/>'";
 
