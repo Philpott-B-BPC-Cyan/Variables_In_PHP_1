437aa5ca233b654444a531829f2e7343c3497b0c
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-20 09:48:31 +0100

    Update Variable.php

--- Variable.php
+++ Variable.php
@@ -3,20 +3,16 @@
 <Body>
 
 <?php
+  $myfirst-"Hello world of course!";
+  $firstNum=6;$FirstFloat=12.5;
+  .
+  echo$myfirst;
+  echo"<br>";
+  echo$firstNum;
+  echo"<br>";
+  echo$FirstFloat;
+ ?>
 
-$myText1="Good Morning Campers";
-$myText2="Suits you sir";
-
-
-
-echo "The first string is:   ";
-echo $myText1;
-echo "'<br/><br/>'";
-echo "The second string is:  ";
-echo $myText2;
-echo "'<br/><br/>'";
-
-?>
 
 </Body>
 
