ebd932ac006e875cb97fed6816503e07f8a3ee8f
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-20 09:49:54 +0100

    Update Variable.php

--- Variable.php
+++ Variable.php
@@ -3,14 +3,14 @@
 <Body>
 
 <?php
-  $myfirst-"Hello world of course!";
+  $myfirst="Hello world of course!";
   $firstNum=6;$FirstFloat=12.5;
   .
-  echo$myfirst;
-  echo"<br>";
-  echo$firstNum;
-  echo"<br>";
-  echo$FirstFloat;
+  echo $myfirst;
+  echo "<br>";
+  echo $firstNum;
+  echo "<br>";
+  echo $FirstFloat;
  ?>
 
 
